009bb3b6835cb87c1e6377772c7c5aa777d76e2c
LoveTangNow <liujiachengzhenniu@qq.com>
2016-07-28 16:56:11 +0800

    Initial Commit

---
+++ LALA/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  LALA
+//
+//  Created by Thomas Liu on 16/7/28.
+//  Copyright © 2016年 Thomas Liu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
