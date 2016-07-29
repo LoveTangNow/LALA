84176d800f712f81236cbe0edce09a27b79b88d8
LoveTangNow <liujiachengzhenniu@qq.com>
2016-07-29 14:16:53 +0800

    SecondSon可以滑动返回了

---
+++ LALA/Assets.xcassets/SecondSonViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SecondSonViewController.swift
+//  LALA
+//
+//  Created by Thomas Liu on 16/7/29.
+//  Copyright © 2016年 Thomas Liu. All rights reserved.
+//
+
+import UIKit
+
+class SecondSonViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationController!.interactivePopGestureRecognizer!.enabled = true;
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
