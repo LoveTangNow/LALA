809d4acba51fe1354fcf622339caa8194271f57f
LoveTangNow <liujiachengzhenniu@qq.com>
2016-08-04 22:13:45 +0800

    添加载入页面控制。。文件

---
+++ LALA/LaunchUIViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LaunchUIViewController.swift
+//  LALA
+//
+//  Created by Thomas Liu on 16/8/4.
+//  Copyright © 2016年 Thomas Liu. All rights reserved.
+//
+
+import UIKit
+
+class LaunchUIViewController: UIViewController {
+
+}
