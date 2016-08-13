7c4c6b2b303f105869641613553e1e9b99bf757e
LoveTangNow <liujiachengzhenniu@qq.com>
2016-08-14 06:39:53 +0800

    新建“函数库 ”
    哈哈

---
+++ LALA/funccccccc.swift
@@ -0,0 +1,22 @@
+//
+//  funccccccc.swift
+//  LALA
+//
+//  Created by Thomas Liu on 16/8/14.
+//  Copyright © 2016年 Thomas Liu. All rights reserved.
+//
+
+import Foundation
+
+
+class funccccccc {
+    
+    func InnnnnnnnnCoreDataSimida(A:String) -> Int {
+        return 1
+    }
+    
+    func InnnnnnnnnCoreDataSimida(A:String,B:String) -> Int {
+        return 1
+    }
+
+}
