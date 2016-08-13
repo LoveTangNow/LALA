f5645f147776eb9bef371d40d28688f1dcace20d
LoveTangNow <liujiachengzhenniu@qq.com>
2016-08-14 06:18:34 +0800

    妄图添加一根“/”解决 真机 不可更改plist文件的尴尬局面，事实是失败了。哈哈

--- LALA/FThirdViewController.swift
+++ LALA/FThirdViewController.swift
@@ -36,15 +36,16 @@ class FThirdViewController: UIViewController {
     @IBAction func CoreDaraClick(sender: AnyObject) {
         print("点击了plist")
         //读取数据
-        let diaryList:String = NSBundle.mainBundle().pathForResource("Shi_Fou_Deng_Lu", ofType:"plist")!
+        let diaryList:String = NSBundle.mainBundle().pathForResource("/Shi_Fou_Deng_Lu", ofType:"plist")!
         let data:NSMutableDictionary = NSMutableDictionary(contentsOfFile:diaryList)!
         let da:NSString = data.objectForKey("username") as! NSString
+        
         print("现在的数据是"+(da as String))
         pl.text = da as String
         
         //写入数据
         //手机上不可实现
-        let plistpath:NSString = NSBundle.mainBundle().pathForResource("Shi_Fou_Deng_Lu", ofType:"plist")!
+        let plistpath:NSString = NSBundle.mainBundle().pathForResource("/Shi_Fou_Deng_Lu", ofType:"plist")!
         let usersdic:NSMutableDictionary = NSMutableDictionary(contentsOfFile:plistpath as String)!
         usersdic.setObject("Tang", forKey: "username")
         usersdic.writeToFile(plistpath as String,atomically: true)
@@ -52,13 +53,13 @@ class FThirdViewController: UIViewController {
         print("把Tang写进去了?")
         print("")
         
-        
     }
 
     //CoreData按钮的点击
     @IBAction func CoreData(sender: AnyObject) {
+        print("点击了Coredata按钮")
 
-            }
+        }
      
     ///////////////////////一下不重要、、/////////////////
     
