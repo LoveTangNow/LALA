c00567c7b3c3aece2766f1d62fe973c835384d39
Thomasliu <liujiachengzhenniu@qq.com>
2016-09-03 10:52:06 +0800

    Add CollectionView 思密达 😝🌶

---
+++ LALA/FFCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  FFCollectionViewCell.swift
+//  LALA
+//
+//  Created by Thomas Liu on 16/9/3.
+//  Copyright © 2016年 Thomas Liu. All rights reserved.
+//
+
+import UIKit
+
+class FFCollectionViewCell: UICollectionViewCell {
+    
+}
