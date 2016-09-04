2ad8a255ca0c46bb202f817b8181deac3fef3807
Thomasliu <liujiachengzhenniu@qq.com>
2016-09-04 08:18:23 +0800

    全新的布局 全新的2、3

--- LALA/FFrist23TableViewCell.swift
+++ LALA/FFrist23TableViewCell.swift
@@ -37,22 +37,28 @@ class FFrist23TableViewCell: UITableViewCell {
     @IBOutlet weak var image_left_top: UIImageView!
     @IBOutlet weak var image_right_top: UIImageView!
     
-    @IBOutlet weak var image_mid_left: UIImageView!
-    @IBOutlet weak var image_mid_mid: UIImageView!
-    @IBOutlet weak var image_mid_right: UIImageView!
-
-    @IBOutlet weak var image_bottom: UIImageView!
+    @IBOutlet weak var UIImageView_Main1: UIImageView!
+    @IBOutlet weak var UIImageView_Main2: UIImageView!
+    @IBOutlet weak var UIImageView_Main3: UIImageView!
+    
+    @IBOutlet weak var UIImageView_BackGround: UIImageView!
     
     @IBOutlet weak var lable_sender: UILabel!
-    @IBOutlet weak var lable_time: UILabel!
-    
-    var detail_height:Int = 0
-    
-    let height_without_detail = 5 + 30 + 5 + 5 + 90 + 5 + 30 + 5 + 20 + 5
-    // 顶部间隔 + 第一行 + 间隔 + 具体文字? + 间隔 + 中央图片一行 + 间隔 + 评论行 + 间隔 +分界条 + 底部间隔
-    
+    @IBOutlet weak var UILabel_Time: UILabel!
+    @IBOutlet weak var UILabel_Detail: UILabel!
+    @IBOutlet weak var UILabel_Pinglun: UILabel!
+    @IBOutlet weak var UILabel_Zan: UILabel!
+    @IBOutlet weak var UILabel_Cai: UILabel!
+
+    let Guding_Height = 80
 
     override func awakeFromNib() {
+        image_left_top.layer.cornerRadius = image_left_top.frame.width / 2
+        image_left_top.clipsToBounds = true
+        
+        image_right_top.layer.cornerRadius = image_right_top.frame.width / 6
+        image_right_top.clipsToBounds = true
+        
         super.awakeFromNib()
         // Initialization code
     }
