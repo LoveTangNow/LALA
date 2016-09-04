c9917ba062be5a906054a48f68c5521efd56bf08
Thomasliu <liujiachengzhenniu@qq.com>
2016-09-04 09:31:46 +0800

    全新的4.5.6.

--- LALA/FFrist456TableViewCell.swift
+++ LALA/FFrist456TableViewCell.swift
@@ -8,32 +8,6 @@
 
 import UIKit
 
-struct a456TableViewCell
-{
-    let style = "Suit_4_5_6_photos"
-    
-    var detail_height = 0
-    
-    let height_without_detail = 5 + 30 + 5 + 5 + 90 + 5 + 90 + 5 + 30 + 5 + 20 + 5
-    // 顶部间隔 + 第一行 + 间隔 + 具体文字? + 间隔 + 中央图片一行 + 间隔 + 中央图片2行 + 间隔 + 评论行 + 间隔 +分界条 + 底部间隔
-    
-    let Image_top_left:String
-    let Image_top_right: String
-    
-    let Image1: String
-    let Image2: String
-    let Image3: String
-    let Image4: String
-    let Image5: String
-    let Image6: String
-    
-    let Image_Bottom: String
-    
-    let Lable_sender: String
-    let Lable_others: String
-}
-
-
 class FFrist456TableViewCell: UITableViewCell {
     
     @IBOutlet weak var Image_top_left: UIImageView!
@@ -44,17 +18,31 @@ class FFrist456TableViewCell: UITableViewCell {
     @IBOutlet weak var Image4: UIImageView!
     @IBOutlet weak var Image5: UIImageView!
     @IBOutlet weak var Image6: UIImageView!
-    @IBOutlet weak var Image_Bottom: UIImageView!
+    
+    @IBOutlet weak var UIImageView_Background: UIImageView!
     
     @IBOutlet weak var Lable_sender: UILabel!
+    @IBOutlet weak var Label_Time: UILabel!
     @IBOutlet weak var Lable_others: UILabel!
     
-    var detail_height = 0
+    @IBOutlet weak var UILabel_pinglun: UILabel!
+    @IBOutlet weak var UILabel_zan: UILabel!
+    @IBOutlet weak var UILabel_cai: UILabel!
+    
+    
+    let Guding_Height = 80
     
     let height_without_detail = 5 + 30 + 5 + 5 + 90 + 5 + 90 + 5 + 30 + 5 + 20 + 5
     // 顶部间隔 + 第一行 + 间隔 + 具体文字? + 间隔 + 中央图片一行 + 间隔 + 中央图片2行 + 间隔 + 评论行 + 间隔 +分界条 + 底部间隔
 
     override func awakeFromNib() {
+        
+        Image_top_left.layer.cornerRadius = Image_top_left.frame.width / 2
+        Image_top_left.clipsToBounds = true
+        
+        Image_top_right.layer.cornerRadius = Image_top_right.frame.width / 6
+        Image_top_right.clipsToBounds = true
+        
         super.awakeFromNib()
         // Initialization code
     }
