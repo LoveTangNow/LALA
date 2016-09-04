fb22b48daf12eba07357f85d361655a75e816ec9
Thomasliu <liujiachengzhenniu@qq.com>
2016-09-04 09:32:15 +0800

    不必要的 import 结构体 删删删

--- LALA/FFrist23TableViewCell.swift
+++ LALA/FFrist23TableViewCell.swift
@@ -8,30 +8,6 @@
 
 import UIKit
 
-
-
-//Table的Cell控制文件
-struct timecell {
-    let style = "Suit_2_3_photos"
-    var detail_height:Int = 0
-    
-    let height_without_detail = 5 + 30 + 5 + 5 + 90 + 5 + 30 + 5 + 20 + 5
-    // 顶部间隔 + 第一行 + 间隔 + 具体文字? + 间隔 + 中央图片一行 + 间隔 + 评论行 + 间隔 +分界条 + 底部间隔
-    
-    
-    let image_left_top: String
-    let image_right_top: String
-    
-    let image_mid_left: String
-    let image_mid_mid: String
-    let image_mid_right: String
-    
-    let image_bottom: String
-    
-    let lable_sender: String
-    let lable_time: String
-}
-
 class FFrist23TableViewCell: UITableViewCell {
     
     @IBOutlet weak var image_left_top: UIImageView!
--- LALA/FFrist_1_big_TableViewCell.swift
+++ LALA/FFrist_1_big_TableViewCell.swift
@@ -6,30 +6,6 @@
 //  Copyright © 2016年 Thomas Liu. All rights reserved.
 //
 import UIKit
-import AVKit
-import AVFoundation
-import AlamofireImage
-import Alamofire
-import CoreData
-import Foundation
-import SwiftyJSON
-
-
-
-struct TableViewCell_1_big {
-    let style = "Suit_1_big_photos"
-    var detail_height = 0
-    
-    
-    let height_without_detail = 5 + 30 + 5 + 0 +  5 + 30 + 5 + 20 + 5
-    // 顶部间隔 + 第一行 + 间隔 + 具体文字?  + 间隔 + 评论行 + 间隔 +分界条 + 底部间隔
-    
-    let UIImageView_Top_Left:UIImage
-    let UIImageView_Top_Right:UIImage
-    let UIImageView_Main:UIImage
-    let UIImageView_Bottom:UIImage
-
-}
 
 class FFrist_1_big_TableViewCell: UITableViewCell {
     
@@ -50,10 +26,6 @@ class FFrist_1_big_TableViewCell: UITableViewCell {
     @IBOutlet weak var ConteViewssssss: UIView!
 
     let Guding_Height = 80
-
-    var Imageloads:Image = UIImage(named: "Black.png")!
-    var ImageloadBackGroud:Image = UIImage(named: "FirstBackGround.png")!
-    
     
     override func awakeFromNib() {
         
@@ -63,8 +35,6 @@ class FFrist_1_big_TableViewCell: UITableViewCell {
         UIImageView_Top_Right.layer.cornerRadius = UIImageView_Top_Right.frame.width / 6
         UIImageView_Top_Right.clipsToBounds = true
         
-        //UIImageView_BackGround.image = ImageloadBackGroud
-
         super.awakeFromNib()
         // Initialization code
     }
