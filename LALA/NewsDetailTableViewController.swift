a022c6325d7b87c7b43e409a0094547d87f00efc
Thomasliu <liujiachengzhenniu@qq.com>
2016-09-05 16:22:03 +0800

    初步展示一点东西：动态详情页。

--- LALA/NewsDetailTableViewController.swift
+++ LALA/NewsDetailTableViewController.swift
@@ -10,9 +10,48 @@ import UIKit
 
 class NewsDetailTableViewController: UITableViewController {
 
+    var newsid:String = ""
+    var newstime:String = ""
+    var device:String = ""
+    var sendername:String = ""
+    var detail:String = ""
+    
+    var Tableviewcell_Height:CGFloat = 0
+    var Device_Width:CGFloat = 0
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        newsid = NSUserDefaults.standardUserDefaults().valueForKey("newsid") as! String
+        newstime = NSUserDefaults.standardUserDefaults().valueForKey("newstime") as! String
+        device = NSUserDefaults.standardUserDefaults().valueForKey("device") as! String
+        sendername = NSUserDefaults.standardUserDefaults().valueForKey("sendername") as! String
+        detail = NSUserDefaults.standardUserDefaults().valueForKey("detail") as! String
+        
+        Device_Width = UIScreen.mainScreen().bounds.width
+        
+        /*
+         用户 id senderid
+         用户 昵称  sendername
+         消息的 时间 设备 senddevice
+         消息的 长文字 detail
+         消息的图片 缓存信息  ?????
+         
+         根据用户 id 去查询 评论   ????
+         */
+        //得到用户 信息 之后呢 发起网络请求那就呗
+        /*
+         320
+         375.5
+         414
+         */
+        
+        print(Device_Width)
+        print(LoadTableViewCells().Height_Work("你好LALA", Width:Device_Width))
+        print(LoadTableViewCells().getDeviceVersion())
+        
+        self.title = "消息"
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -27,69 +66,74 @@ class NewsDetailTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
+    
+    //TableView中有几个Sections
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 3
+        /*
+         1、动态
+            1、上部分
+            2、图片部分
+         2、广告
+         3、评论
+         */
     }
-
+    
+    //Section中有几行 tableview
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        if section == 0 {
+            //动态
+            return 2
+        }
+        if section == 1 {
+            //广告
+            return 1
+        }
+        else{
+            //评论
+            return 10
+        }
+        
     }
-
-    /*
+    
+    
+    //cell的初始化都在这里了 返回 一个cell
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+        /*
+         1、动态
+         1、上部分
+         2、图片部分
+         2、广告
+         3、评论
+         */
+        if indexPath.section == 0 && indexPath.row == 0  {
+            let cell = tableView.dequeueReusableCellWithIdentifier("UserTableViewCell", forIndexPath: indexPath) as! UserTableViewCell
+            cell.UILabel_SenderName.text = sendername
+            cell.UILabel_Detail.text = detail
+            cell.UILabel_News_Information.text = newstime + " " + device
+            // Configure the cell...
+            /*
+             判断设备
+             根据 detail 文字的个数来计算需要的高度
+                固定高度 和 非固定的 lable 的高度
+             */
+            
+            Tableviewcell_Height = cell.Guding_Height + CGFloat(LoadTableViewCells().Height_Work(detail, Width: UIScreen.mainScreen().bounds.width))
+            
+            return cell
 
+        }
+        let cell = tableView.dequeueReusableCellWithIdentifier("UserTableViewCell", forIndexPath: indexPath) as! UserTableViewCell
         // Configure the cell...
-
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat
+    {
+        //print("Tableview cell高度")
+        return Tableviewcell_Height
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
