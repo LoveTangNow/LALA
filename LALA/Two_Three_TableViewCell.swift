46bf7815a41e3cd9b2d2ce5652dd349124704be4
Thomasliu <liujiachengzhenniu@qq.com>
2016-09-05 12:56:51 +0800

    2.3 图片TableViewCell

---
+++ LALA/Two_Three_TableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  Two_Three_TableViewCell.swift
+//  LALA
+//
+//  Created by Thomas Liu on 16/9/5.
+//  Copyright © 2016年 Thomas Liu. All rights reserved.
+//
+
+import UIKit
+
+class Two_Three_TableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var UIImageView1: UIImageView!
+    @IBOutlet weak var UIImageView2: UIImageView!
+    @IBOutlet weak var UIImageView3: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
