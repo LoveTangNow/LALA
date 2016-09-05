f697109cc2d6daee3a05707f00ffd6829373d498
Thomasliu <liujiachengzhenniu@qq.com>
2016-09-05 12:57:16 +0800

    添加 7.8.9 TableViewCell

---
+++ LALA/Seven_Eight_Nine_TableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  Seven_Eight_Nine_TableViewCell.swift
+//  LALA
+//
+//  Created by Thomas Liu on 16/9/5.
+//  Copyright © 2016年 Thomas Liu. All rights reserved.
+//
+
+import UIKit
+
+class Seven_Eight_Nine_TableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var UIImageView1: UIImageView!
+    @IBOutlet weak var UIImageView2: UIImageView!
+    @IBOutlet weak var UIImageView3: UIImageView!
+    @IBOutlet weak var UIImageView4: UIImageView!
+    @IBOutlet weak var UIImageView5: UIImageView!
+    @IBOutlet weak var UIImageView6: UIImageView!
+    @IBOutlet weak var UIImageView7: UIImageView!
+    @IBOutlet weak var UIImageView8: UIImageView!
+    @IBOutlet weak var UIImageView9: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
