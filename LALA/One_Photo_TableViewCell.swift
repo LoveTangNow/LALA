c1a57f6d49b96883d925d37c12039b65ed0a67d2
Thomasliu <liujiachengzhenniu@qq.com>
2016-09-05 12:59:57 +0800

    添加 1 图片 TableViewCell

---
+++ LALA/One_Photo_TableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  OnePhotoTableViewCell.swift
+//  LALA
+//
+//  Created by Thomas Liu on 16/9/5.
+//  Copyright © 2016年 Thomas Liu. All rights reserved.
+//
+
+import UIKit
+
+class One_Photo_TableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var UIImageView_Main: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
