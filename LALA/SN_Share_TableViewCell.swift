ddac91caad4417eb80b5021a2047d4d64f136696
Thomasliu <liujiachengzhenniu@qq.com>
2016-09-06 17:29:03 +0800

    updates

--- LALA/SN_Share_TableViewCell.swift
+++ LALA/SN_Share_TableViewCell.swift
@@ -8,19 +8,30 @@
 
 import UIKit
 
-class SN_SHARE_TableViewCell: UITableViewCell {
+class SN_SHARE_TableViewCell: UITableViewCell,UINavigationControllerDelegate,UIImagePickerControllerDelegate {
     
     @IBOutlet weak var UIImageView1: UIImageView!
+    
+    
+    
+    @IBAction func Button_Click(sender: AnyObject) {
+      
+        print("aaaaaa")
+        
+    }
 
+    
+        
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
+
 
 }
