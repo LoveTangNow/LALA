0fd3f0c640a8ca409dcf0966d7dbd07370f1ab22
Thomasliu <liujiachengzhenniu@qq.com>
2016-09-06 11:13:50 +0800

    菊🌹转啊转

--- LALA/FSecondViewController.swift
+++ LALA/FSecondViewController.swift
@@ -7,13 +7,11 @@
 //
 
 import UIKit
-import AVKit
-import AVFoundation
 import AlamofireImage
 import Alamofire
 import CoreData
-import Foundation
 import SwiftyJSON
+import SVProgressHUD
 
 //Tab bar消息页面
 class FSecondViewController: UIViewController {
@@ -36,6 +34,22 @@ class FSecondViewController: UIViewController {
             .instantiateViewControllerWithIdentifier("ImageUViewController")
         self.navigationController?.pushViewController(vc, animated: true)
     }
+    
+    @IBAction func UIButton_Zhuan_Click(sender: AnyObject) {
+
+        SVProgressHUD.setDefaultStyle(SVProgressHUDStyle.Dark)//前后颜色
+        SVProgressHUD.setDefaultAnimationType(SVProgressHUDAnimationType.Native)//菊花
+        //SVProgressHUD.setDefaultMaskType(SVProgressHUDMaskType.Black)
+        
+        SVProgressHUD.show()
+        //SVProgressHUD.showSuccessWithStatus("lala")
+        //SVProgressHUD.showErrorWithStatus("haha")
+        
+    }
+    @IBAction func UIButton_Ting_Click(sender: AnyObject) {
+        SVProgressHUD.dismiss()
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -61,5 +75,6 @@ class FSecondViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    //文本提示框
+    
 }
