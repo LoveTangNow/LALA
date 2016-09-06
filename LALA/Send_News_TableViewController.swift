13e7120e618820ea436e30554febfa70b257dbaa
Thomasliu <liujiachengzhenniu@qq.com>
2016-09-06 09:25:20 +0800

    我什么都不想写

--- LALA/Send_News_TableViewController.swift
+++ LALA/Send_News_TableViewController.swift
@@ -37,21 +37,57 @@ class Send_News_TableViewController: UITableViewController {
     //TableView中有几个Sections
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 2
     }
     
     //Section中有几行 tableview
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 2
     }
     
     
     //cell的初始化都在这里了 返回 一个cell
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-        // Configure the cell...
-        return cell
+        
+        /*
+         1、
+            1、文字框
+            2、图片选择
+         2、
+            1、地点
+            2、分享
+         */
+        
+        if indexPath.section == 0 {
+            if indexPath.rows == 0 {
+                //文字框
+                let cell = tableView.dequeueReusableCellWithIdentifier("SN_words_TableViewCell", forIndexPath: indexPath) as! SN_words_TableViewCell
+                // Configure the cell...
+                return cell
+            }
+            else{
+                //图片选择
+            }
+        }
+        else{
+            if indexPath.rows == 0 {
+                //地点
+                <#code#>
+            }
+            else{
+                //分享
+            }
+        }
+        
+    }
+    
+    override func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 10
+    }
+    
+    override func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 10
     }
     
     override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat
