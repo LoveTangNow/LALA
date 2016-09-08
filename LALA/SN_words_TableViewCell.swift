dc57d55c4ed67f08db423f6cd57ad278ff266d06
Thomasliu <liujiachengzhenniu@qq.com>
2016-09-08 13:45:04 +0800

    文字控制

--- LALA/SN_words_TableViewCell.swift
+++ LALA/SN_words_TableViewCell.swift
@@ -10,17 +10,33 @@ import UIKit
 
 class SN_WORDS_TableViewCell: UITableViewCell {
     
-    @IBOutlet weak var UITextView1: UITextView!
+    @IBOutlet weak var UITextView_Main: UITextView!
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        if text.containsString("\n") {
+            self.UITextView_Main.endEditing(true)
+            return false
+        }
+        return true
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+        if selected {
+            //选中。
+            print("textview line seleccted")
+        } else {
+            //不再选中。
+            print("textview line no seleccted")
+        }
     }
 
 }
