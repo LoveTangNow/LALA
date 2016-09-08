940dcd9f8f1ea7814aa9fe11e2df17c67d5997bf
Thomasliu <liujiachengzhenniu@qq.com>
2016-09-08 13:44:49 +0800

    评论控制

--- LALA/Page1/cells/PinglunZanO_TableViewCell.swift
+++ LALA/Page1/cells/PinglunZanO_TableViewCell.swift
@@ -3,7 +3,7 @@
 //  LALA
 //
 //  Created by Thomas Liu on 16/9/7.
-//  Copyright © 2016年 Thomas Liu. All rights reserved.
+//  Copyright © 2016 Thomas Liu. All rights reserved.
 //
 
 import UIKit
@@ -14,13 +14,56 @@ class PinglunZanO_TableViewCell: UITableViewCell {
     @IBOutlet weak var UIButton2: UIButton!
     @IBOutlet weak var UIButton3: UIButton!
     
+    var Pinglun_NUM = 0
+    var Zan_NUM = 0
+    var Cai_NUM = 0
+    
+    var Pinglun_BOOL = false
+    var Zan_BOOL = false
+    var Cai_BOOL = false
+    
+    var Sender_ID:String = ""
+    
+    
+    //点赞取消赞还需要同步到服务器上啊~！！
+    
     @IBAction func UIButton1_Click(sender: AnyObject) {
+        //评论 需要转页面
+        if Pinglun_BOOL == false {
+            Pinglun_NUM += 1
+            Pinglun_BOOL = true
+        } else {
+            Pinglun_NUM -= 1
+            Pinglun_BOOL = false
+        }
+        
+        UIButton1.setTitle("评论" + String(Pinglun_NUM), forState:.Normal)
     }
     
     @IBAction func UIButton2_Click(sender: AnyObject) {
+        //赞
+        if Zan_BOOL == false {
+            Zan_NUM += 1
+            Zan_BOOL = true
+        } else {
+            Zan_NUM -= 1
+            Zan_BOOL = false
+        }
+
+        UIButton2.setTitle("赞" + String(Zan_NUM), forState:.Normal)
     }
     
     @IBAction func UIButton3_Click(sender: AnyObject) {
+        //踩
+        if Cai_BOOL == false {
+            Cai_NUM += 1
+            Cai_BOOL = true
+        } else {
+            Cai_NUM -= 1
+            Cai_BOOL = false
+        }
+
+        UIButton3.setTitle("踩" + String(Cai_NUM), forState:.Normal)
     }
 
     override func awakeFromNib() {
