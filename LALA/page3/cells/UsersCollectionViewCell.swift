14e1a5de2bd513b8dcd708982b19d290bfd5c608
Thomasliu <liujiachengzhenniu@qq.com>
2016-09-12 23:33:29 +0800

    UsersCollectionViewCell
    包括一个图片 和一个 文字 label
    盛放 usericon 和 username

---
+++ LALA/page3/cells/UsersCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  UsersCollectionViewCell.swift
+//  LALA
+//
+//  Created by Thomas Liu on 16/9/12.
+//  Copyright © 2016年 Thomas Liu. All rights reserved.
+//
+
+import UIKit
+
+class UsersCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var UIImageView_M: UIImageView!
+    
+    @IBOutlet weak var UILabel_M: UILabel!
+}
